3b9e248f77887a2cbb7055193dabc6b46006a327
learning <learning@DIN66007358.corp.capgemini.com>
2018-07-06 17:48:45 +0530

    edfib

--- Dummy/src/com/cg/DummyDemo.java
+++ Dummy/src/com/cg/DummyDemo.java
@@ -5,6 +5,8 @@ public class DummyDemo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello ");
+		System.out.println("LALALALAL");
 		System.out.println("Welcome to dummy");
 	}
+	
 }
